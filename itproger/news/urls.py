e1e60dfc1be453731854e5a403bd804d466a3822
Lucius <146953207+BearLucius@users.noreply.github.com>
2024-12-18 19:46:12 +0300

    Merge pull request #1
    * Test №2
    
    * Test №3
    
    * Test №4 - done

--- itproger/news/urls.py
+++ itproger/news/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('main.urls')),
-    path('admin/', admin.site.urls),
-    path('news/', include('news.urls')),
-] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+path('', views.news_home, name='news_home'),
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail'),
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name='news-update'),
+    path('<int:pk>/delete', views.NewsDeleteView.as_view(), name='news-delete'),
+
+]
